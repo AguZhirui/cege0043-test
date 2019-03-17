a4d6bcfbf1e7f3ff10d09d23790ff602e4da3884
AguZhirui <uceszg0@ucl.ac.uk>
2019-03-17 16:51:09 -0400

    correct files

--- testAJAX.js
+++ testAJAX.js
@@ -1,11 +1,11 @@
 var xhr; // define the global variable to process the AJAX request
 function callDivChange() {
 xhr = new XMLHttpRequest();
-var filename = document.getElementById("filename").value;
-xhr.open("GET", filename, true);
+xhr.open("GET", "test.html", true);
 xhr.onreadystatechange = processDivChange;
 xhr.send();
 }
+
 function processDivChange() {
 if (xhr.readyState < 4) // while waiting response from
 server
